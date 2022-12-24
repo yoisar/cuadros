502141350a52860f780b267736c3ab1252c3d396
YOIS <sioy23@gmail.com>
2022-12-24 16:42:38 -0300

    add pictures relationships

--- api/v1/app/Models/Picture.php
+++ api/v1/app/Models/Picture.php
@@ -8,4 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class Picture extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description', 'image', 'category_id', 'painter_id', 'dimension_id'
+    ];
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function painter()
+    {
+        return $this->belongsTo(Painter::class);
+    }
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function dimension()
+    {
+        return $this->belongsTo(Dimension::class);
+    }
 }
